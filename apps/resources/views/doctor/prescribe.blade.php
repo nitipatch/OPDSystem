bdce91f79f938e7affee8bde578ad0a108a404bf
top106 <top1062p@gmail.com>
2015-11-04 13:12:23 +0700

    ฝากทำให้เป็น list

--- apps/resources/views/doctor/prescribe.blade.php
+++ apps/resources/views/doctor/prescribe.blade.php
@@ -9,66 +9,67 @@
 		])
 !!} 
 
-		<h2 class="text-center">Prescribe rev 0.1 alpha</h2>
+<h2 class="text-center">Prescribe rev 0.1 alpha</h2>
+
+<!-- header -->
+<!-- <form action="" method="POST" class="form-horizontal" role="form">
+
+		<img src="http://www.online-image-editor.com//styles/2014/images/example_image.png" class="img-responsive" alt="Image">
+
+</form> -->
+
+
+<!-- old -->
+<div class="row">
 		
-		<!-- header -->
-		<!-- <form action="" method="POST" class="form-horizontal" role="form">
+	<form action="" method="POST" role="form">
+		<legend>โปรดกรอกข้อมูลการสั่งยา</legend>
 
-				<img src="http://www.online-image-editor.com//styles/2014/images/example_image.png" class="img-responsive" alt="Image">
-
-		</form> -->
-
-
-		<!-- old -->
+		<div class="form-group">
+			<!-- <label for="">Name</label> -->
+			<!-- <input type="text" class="form-control" name="Name" id="input" required="required" title="Name" placeholder="Name"> -->
+			
+			
 			<div class="row">
-					
-				<form action="" method="POST" role="form">
-					<legend>โปรดกรอกข้อมูลการสั่งยา</legend>
-
-					<div class="form-group">
-						<!-- <label for="">Name</label> -->
-						<!-- <input type="text" class="form-control" name="Name" id="input" required="required" title="Name" placeholder="Name"> -->
-						
-						
-						<div class="row">
-							<div class="col-lg-5">
-								ชื่อยา<font color="red">*</font>
-								<input type="text" name="name" id="input" class="form-control" value="" required="required" pattern="" title="">
-							</div>
-						</div>
-						
-						<p></p>
-
-						<div class="row">
-							<div class="col-lg-5">
-								จำนวน<font color="red">*</font>
-								<input type="text" name="quantity" id="input" class="form-control" value="" required="required" pattern="" title="">
-							</div>
-						</div>
-						
-						
-
-						<p></p>
-
-						<div class="row">
-							<div class="col-lg-5">
-								วิธีใช้<font color="red">*</font>
-								<textarea name="usage" id="input" class="form-control" rows="0" required="required"></textarea>
-							</div>
-						</div>
-						
-						<p></p>
-
-					</div>
-				
-				
-				
-				{!!
-				Form::submit('สั่งยา', ['class' => 'btn btn-primary'])
-				!!}
-				<!-- <button type="submit" class="btn btn-primary">Submit</button> -->
-				
-
-				</form>
+				<div class="col-lg-5">
+					ชื่อยา<font color="red">*</font>
+					<input type="text" name="name" id="input" class="form-control" value="" required="required" pattern="" title="">
+				</div>
 			</div>
+			
+			<p></p>
+
+			<div class="row">
+				<div class="col-lg-5">
+					จำนวน<font color="red">*</font>
+					<input type="text" name="quantity" id="input" class="form-control" value="" required="required" pattern="" title="">
+				</div>
+			</div>
+			
+			
+
+			<p></p>
+
+			<div class="row">
+				<div class="col-lg-5">
+					วิธีใช้<font color="red">*</font>
+					<textarea name="usage" id="input" class="form-control" rows="0" required="required"></textarea>
+				</div>
+			</div>
+			
+			<p></p>
+
+		</div>
+	
+	<br>
+	
+	
+	{!!
+	Form::submit('สั่งยา', ['class' => 'btn btn-primary'])
+	!!}
+	<!-- <button type="submit" class="btn btn-primary">Submit</button> -->
+	
+
+	</form>
+</div>
 @stop
