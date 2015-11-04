8968b5e0ebf6a2d06ec54f6f7d3caedf7bfe53af
top106 <top1062p@gmail.com>
2015-11-04 22:28:38 +0700

    no message

--- apps/resources/views/patient/makeAppointment.blade.php
+++ apps/resources/views/patient/makeAppointment.blade.php
@@ -15,7 +15,8 @@
         <h3 style="color:red;text-align:center;"></h3>
         <table align="center">
             <tr>
-                <td style="text-align:right;" valign="top"><label>สาเหตุหรืออาการที่ต้องการพบแพทย์</label></td>
+                <td style="text-align:right;" valign="top">
+                    <label>สาเหตุหรืออาการที่ต้องการพบแพทย์<font color="red">*</font></label></td>
                 <td>
                     {!! Form::textarea('cause', Input::old('กรอกสาเหตุหรืออาการ'), [
                         'placeholder' => 'สาเหตุหรืออาการที่ต้องการพบแพทย์',
@@ -30,7 +31,8 @@
                 </td>
             </tr>
             <tr>
-                <td style="text-align:right;" valign="top"><label>ชื่อแพทย์</label></td>
+                <td style="text-align:right;" valign="top">
+                    <label>ชื่อแพทย์ที่ต้องการพบ<font color="red">*</font></label></td>
                 <td>
                     {!! Form::text('doctor', Input::old('กรอกชื่อแพทย์'), [
                         'placeholder' => 'ชื่อแพทย์',
@@ -45,9 +47,10 @@
                 </td>
             </tr>
             <tr>
-                <td style="text-align:right;" valign="top"><label>แผนก</label></td>
+                <td style="text-align:right;" valign="top">
+                    <label>แผนกของแพทย์ที่ต้องการพบ<font color="red">*</font></label></td>
                 <td>
-                    {!! Form::select('department', ['','อายุรกรรม','ศัลยกรรม','ออร์โธปีดิกส์','กุมารเวชกรรม','สูตินรีเวช'
+                    {!! Form::select('department', ['--เลือกแผนก--','อายุรกรรม','ศัลยกรรม','ออร์โธปีดิกส์','กุมารเวชกรรม','สูตินรีเวช'
                     ,'ทันตกรรม','เวชปฏิบัติ','แพทย์เฉพาะทางอื่นๆ']) !!}
                     @if ($errors->has('doctor'))
                         <p style="color:red;font-size:14px;margin:0;padding:10px 0px;">
@@ -57,7 +60,8 @@
                 </td>
             </tr>  
             <tr>
-                <td style="text-align:right;" valign="top"><label>วันนัด</label></td>
+                <td style="text-align:right;" valign="top">
+                    <label>วันนัด<font color="red">*</font></label></td>
                 <td>
                     {!! Form::date('apptDate', \Carbon\Carbon::now()) !!}
                     @if ($errors->has('apptDate'))
@@ -68,7 +72,8 @@
                 </td>
             </tr>
             <tr>
-                <td style="text-align:right;" valign="top"><label>ช่วงเวลา</label></td>
+                <td style="text-align:right;" valign="top">
+                    <label>ช่วงเวลา<font color="red">*</font></label></td>
                 <td>
                     {!! Form::radio('morning',1) !!}เช้า
                     {!! Form::radio('morning',0) !!}บ่าย
@@ -79,10 +84,10 @@
                     @endif
                 </td>
             </tr>
-            
+            <tr>
             <tr>
                 <td style="text-align:right;"></td>
-                <td>{!! Form::submit('Create', ['class' => 'btn']) !!}</td>
+                <td>{!! Form::submit('ทำนัด', ['class' => 'btn']) !!}</td>
             </tr>
         </table>
     </div>
